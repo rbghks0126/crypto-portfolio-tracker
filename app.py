cc1e39d3f1b59ad5a6d55297f841b0f73ba14fee
rbghks0126 <40514272+rbghks0126@users.noreply.github.com>
2022-01-14 22:32:26 +1100

    fix coin categories

--- app.py
+++ app.py
@@ -11,61 +11,61 @@ def gsheet_coin_tracker():
 
     # 70 tokens from cryptobanter's youtube video
     target_tokens_list = ['btc', 'eth', 'sol', 'ada', 'luna', 'dot', 'avax', 'matic', 'algo',
-                        'ftm', 'uni', 'egld', 'one', 'ar', 'ksm', 'rune', 'rose', 'cel',
-                        'sushi', 'jewel', 'spell', 'yfi', 'deso', 'tshare', 'audio',
-                        'flux', 'rndr', 'dydx', 'anc', 'tomb', 'any', 'movr', 'ufo',
-                        'metis', 'boba', 'ygg', 'uos', 'vr', 'vader', 'soul', 'super',
-                        'ewt', 'inj', 'akt', 'pyr', 'rmrk', 'joe', 'qrdo', 'ldo', 'astro',
-                        'aioz', 'alu', 'eden', 'stt', 'gog', 'spool', 'unic', 'kuji',
-                        'fara', 'prism', 'realm', 'rin', 'don', 'pickle', 'sunny', 'hol',
-                        'nii', 'uxp', 'spin', 'bbs']
+                          'ftm', 'uni', 'egld', 'one', 'ar', 'ksm', 'rune', 'rose', 'cel',
+                          'sushi', 'jewel', 'spell', 'yfi', 'deso', 'tshare', 'audio',
+                          'flux', 'rndr', 'dydx', 'anc', 'tomb', 'any', 'movr', 'ufo',
+                          'metis', 'boba', 'ygg', 'uos', 'vr', 'vader', 'soul', 'super',
+                          'ewt', 'inj', 'akt', 'pyr', 'rmrk', 'joe', 'qrdo', 'ldo', 'astro',
+                          'aioz', 'alu', 'eden', 'stt', 'gog', 'spool', 'unic', 'kuji',
+                          'fara', 'prism', 'realm', 'rin', 'don', 'pickle', 'sunny', 'hol',
+                          'nii', 'uxp', 'spin', 'bbs']
     token_category = ['sov',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'dex',
-                  'layer 1',
-                  'layer 1',
-                  'web 3',
-                  'layer 1',
-                  'layer 1',
-                  'layer 1',
-                  'dex',
-                  'defi',
-                  'defi',
-                  'defi',
-                  'social',
-                  'defi',
-                  'social',
-                  'web 3',
-                  'web 3',
-                  'dex',
-                  'defi',
-                  'defi',
-                  'dex',
-                  'cross chain',
-                  'layer 1',
-                  'gaming',
-                  'layer 1',
-                  'layer 1',
-                  'gaming',
-                  'gaming/nft',
-                  'gaming/nft',
-                  'defi',
-                  'gaming/nft',
-                  'gaming',
-                  'other',
-                  'cross chain',
-                  'web 3',
-                  'gaming/nft', 'gaming/nft', 'dex', 'defi', 'defi', 'dex', 'web 3', 'gaming/nft', 'defi', 'launchpad', 'gaming/nft',
-                  'defi', 'gaming/nft', 'defi', 'gaming/nft', 'defi', 'gaming/nft', 'dex', 'defi', 'defi', 'defi', 'gaming/nft', 'layer 1',
-                  'defi', 'gaming/nft', 'social']
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'dex',
+                      'layer 1',
+                      'layer 1',
+                      'web 3',
+                      'layer 1',
+                      'layer 1',
+                      'layer 1',
+                      'defi',
+                      'dex',
+                      'defi',
+                      'defi',
+                      'defi',
+                      'social',
+                      'defi',
+                      'social',
+                      'web 3',
+                      'web 3',
+                      'defi',
+                      'defi',
+                      'defi',
+                      'cross chain',
+                      'layer 1',
+                      'gaming',
+                      'layer 1',
+                      'layer 1',
+                      'gaming',
+                      'gaming/nft',
+                      'gaming/nft',
+                      'defi',
+                      'gaming/nft',
+                      'gaming',
+                      'other',
+                      'cross chain',
+                      'web 3',
+                      'gaming/nft', 'gaming/nft', 'dex', 'defi', 'defi', 'defi', 'web 3', 'gaming/nft', 'defi', 'launchpad', 'gaming/nft',
+                      'defi', 'gaming/nft', 'defi', 'gaming/nft', 'defi', 'gaming/nft', 'dex', 'defi', 'defi', 'defi', 'gaming/nft', 'layer 1',
+                      'defi', 'gaming/nft', 'social']
 
     cg = CoinGeckoAPI()
     all_coins_list = cg.get_coins_list()
@@ -106,13 +106,13 @@ def gsheet_coin_tracker():
     df_merge['category'] = token_category
     df_merge = util_functions.calc_days_from_ath(df_merge)
 
-
     df_merge.sort_values(by='rank', inplace=True)
     df_merge.set_index('rank', inplace=True)
 
     gsheet_instance, df_gsheet = gsheet_functions.get_gsheet_df(
         'gsheet_coin_tracker')
-    update_info = [row.tolist() for i, row in df_merge.reset_index().iterrows()]
+    update_info = [row.tolist()
+                   for i, row in df_merge.reset_index().iterrows()]
 
     # update gsheet!
     gsheet_functions.update_gsheet(gsheet_instance, update_info)
